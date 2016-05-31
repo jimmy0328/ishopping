5888e243a852ca17fc42300ec06f539a257f6a1d
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-05-31 16:40:08 +0800

    add landing page layout

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -36,6 +36,10 @@ class ProductImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [50, 50]
   end
 
+  version :cover do
+    process :resize_to_fit => [400, 400]
+  end
+
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
@@ -43,9 +47,9 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_white_list
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
