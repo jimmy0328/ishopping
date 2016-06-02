eec35d3c9ae69515d5fea8e6ca52a517f3f3be94
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-06-02 13:41:01 +0800

    add tagging to admin product

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'bootstrap-sass'
 gem "font-awesome-rails"
+gem 'bootstrap-tagsinput-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   mount_uploader :cover_image, ProductImageUploader
   has_many :product_images, dependent: :destroy
-  validates :title, :description, :cost, :price, :display, presence: true
+  validates :title, :description, :cost, :price, presence: true
 end
