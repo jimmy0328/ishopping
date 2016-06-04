a6e57c333f5b2cbe8cea5acf1a6bef749d99fecb
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-06-04 15:11:36 +0800

    add admin root page

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,5 @@
+class Admin::HomeController < Admin::AdminController
+
+  def index; end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :admin do
+    root "home#index"
     resources :products
   end
 
