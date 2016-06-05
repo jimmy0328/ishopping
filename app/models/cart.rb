e52bdbdf90b1f7dc99cc4b5e6a260e7b6dfaf02e
jimmy <jimmy0328@gmail.com>
2016-06-05 17:31:51 +0800

    Add cart show page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,15 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  has_many :item, through: :cart_items, source: :product
+  has_many :items, through: :cart_items, source: :product
 
   def add(product)
     ci = cart_items.build
     ci.product = product
     ci.save
   end
+
+  def price
+    items.map(&:price).sum
+  end
+
 end
