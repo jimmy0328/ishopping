3c46d76e8f239f4dd1644d0fd6f2682e39857b67
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-06-07 22:04:00 +0800

    add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607044056) do
+ActiveRecord::Schema.define(version: 20160607134918) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160607044056) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
